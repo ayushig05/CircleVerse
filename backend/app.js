50441078440e8488c71be7667e9137d8fec32252
Ayushi Gupta <118197038+ayushig05@users.noreply.github.com>
2025-07-13 02:40:53 +0530

    fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -16,24 +16,29 @@ const app = express();
 app.use("/", express.static("uploads"));
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors({
-    origin: ["http://localhost:5173"],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+        "http://localhost:5173", 
+        "https://circle-verse.vercel.app/"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 app.use(express.json({ limit: "10kb" }));
 app.use((req, res, next) => {
-    if (req.body) req.body = mongoSanitize.sanitize(req.body);
-    if (req.params) req.params = mongoSanitize.sanitize(req.params);
-    if (req.query) req.query = mongoSanitize.sanitize({ ...req.query });
-    next();
+  if (req.body) req.body = mongoSanitize.sanitize(req.body);
+  if (req.params) req.params = mongoSanitize.sanitize(req.params);
+  if (req.query) req.query = mongoSanitize.sanitize({ ...req.query });
+  next();
 });
 
 app.get("/", (req, res) => {
-    res.send("Backend is up and running!");
+  res.send("Backend is up and running!");
 });
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
