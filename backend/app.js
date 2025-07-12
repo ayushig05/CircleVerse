fc166a6c4165a4495ada285c20d817b0d1f5199e
Ayushi Gupta <118197038+ayushig05@users.noreply.github.com>
2025-07-13 02:44:07 +0530

    fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
         "http://localhost:5173", 
-        "https://circle-verse.vercel.app/"
+        "https://circle-verse.vercel.app"
     ],
     credentials: true,
   })
